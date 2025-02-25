44b180a99b97766cfa14626edd283b94536c7685
fsampson30 <fsampson30@gmail.com>
2025-02-25 07:09:32 -0300

    Initial commit

---
+++ src/test/java/com/sampson/dio_api/DioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.dio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
